2e7ac4cfe6007e5f6ba9c65d2e939ccdcb6d1905
Eric_2139 <ericpowercosta2005@gmail.com>
2025-07-03 22:47:13 -0300

    o case 8 agora funciona, mas as pessoas não estão sendo salvas no neo4j

--- src/main/java/com/example/trabalhobancodedados/config/Neo4jConfig.java
+++ src/main/java/com/example/trabalhobancodedados/config/Neo4jConfig.java
@@ -1,11 +1,19 @@
 package com.example.trabalhobancodedados.config;
 
+import org.neo4j.driver.Driver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableNeo4jRepositories(basePackages = "com.example.trabalhobancodedados.repository.neo4j",
 repositoryImplementationPostfix = "Neo4j")
 public class Neo4jConfig {
-    
+
+    @Bean(name = "neo4jTransactionManager")
+    public PlatformTransactionManager neo4jTransactionManager(Driver driver) {
+        return new Neo4jTransactionManager(driver);
+    }
 }
