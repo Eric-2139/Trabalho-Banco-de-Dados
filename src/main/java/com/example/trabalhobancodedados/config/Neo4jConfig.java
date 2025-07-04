42131d1196df079db9d3062f3dd2e529bf371cbd
Eric_2139 <ericpowercosta2005@gmail.com>
2025-07-03 22:01:49 -0300

    as tentativas de fazer o neo4j funcionar continuam

--- src/main/java/com/example/trabalhobancodedados/config/Neo4jConfig.java
+++ src/main/java/com/example/trabalhobancodedados/config/Neo4jConfig.java
@@ -1,20 +1,11 @@
 package com.example.trabalhobancodedados.config;
 
-import org.neo4j.driver.Driver;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableNeo4jRepositories(basePackages = "com.example.trabalhobancodedados.repository.neo4j",
 repositoryImplementationPostfix = "Neo4j")
 public class Neo4jConfig {
-
-    @Bean
-    public PlatformTransactionManager transactionManager(Driver driver) {
-        return new Neo4jTransactionManager(driver);
-    }
     
 }
--- src/main/java/com/example/trabalhobancodedados/config/PostgreConfig.java
+++ src/main/java/com/example/trabalhobancodedados/config/PostgreConfig.java
@@ -1,10 +1,20 @@
 package com.example.trabalhobancodedados.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.example.trabalhobancodedados.repository.postgresql")
 public class PostgreConfig {
     
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 }
